ca1f053bec444aedadf4d9a8e59a1b61aca962f6
MantisMcKoy <acfujimoto@gmail.com>
2019-02-16 14:03:54 -0500

    added button functionality for configuration activity

--- app/src/main/java/edu/jumpstreet/spacetrader/MainActivity.java
+++ app/src/main/java/edu/jumpstreet/spacetrader/MainActivity.java
@@ -1,13 +1,28 @@
 package edu.jumpstreet.spacetrader;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import edu.jumpstreet.spacetrader.view.ConfigurationActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ConstraintLayout layout = findViewById(R.id.mainConstrainLayout);
+        layout.setOnTouchListener(this);
     }
+
+    public boolean onTouch(View v, MotionEvent event){
+        Intent intent = new Intent(this, ConfigurationActivity.class);
+        MainActivity.this.startActivity(intent);
+        return true;
+    }
+
 }
--- app/src/main/java/edu/jumpstreet/spacetrader/view/ConfigurationActivity.java
+++ app/src/main/java/edu/jumpstreet/spacetrader/view/ConfigurationActivity.java
@@ -1,15 +1,170 @@
 package edu.jumpstreet.spacetrader.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import edu.jumpstreet.spacetrader.R;
+import edu.jumpstreet.spacetrader.model.PlayerInteractor;
+import edu.jumpstreet.spacetrader.viewmodel.ConfigurationViewModel;
 
-public class ConfigurationActivity extends AppCompatActivity {
+public class ConfigurationActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText nameET;
+    private static final int MAX_POINTS = 16;
+    private int remainingPoints = MAX_POINTS;
+
+    TextView skillPointsRemainingTV;
+
+    Button pilotMinusBtn;
+    TextView pilotTV;
+    Button pilotPlusBtn;
+    private int pilotSP = 0;
+
+    Button fighterMinusBtn;
+    TextView fighterTV;
+    Button fighterPlusBtn;
+    private int fighterSP = 0;
+
+    Button traderMinusBtn;
+    TextView traderTV;
+    Button traderPlusBtn;
+    private int traderSP = 0;
+
+    Button engineerMinusBtn;
+    TextView engineerTV;
+    Button engineerPlusBtn;
+    private int engineerSP = 0;
+
+    Button confirmationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_configuration);
+
+        linkConfigButtons();
+        initializeButtons();
+        skillPointsRemainingTV.setText("Remaining Skill Points: " + remainingPoints);
+    }
+
+    private void linkConfigButtons(){
+        nameET = findViewById(R.id.nameEditText);
+        pilotMinusBtn = findViewById(R.id.pilotMinusButton);
+        pilotTV = findViewById(R.id.configPilotTextView);
+        pilotPlusBtn = findViewById(R.id.pilotPlusButton);
+        fighterMinusBtn = findViewById(R.id.fighterMinusButton);
+        fighterTV = findViewById(R.id.configFighterTextView);
+        fighterPlusBtn = findViewById(R.id.fighterPlusButton);
+        traderMinusBtn = findViewById(R.id.traderMinusButton);
+        traderTV = findViewById(R.id.configTraderTextView);
+        traderPlusBtn = findViewById(R.id.traderPlusButton);
+        engineerMinusBtn = findViewById(R.id.engineerMinusButton);
+        engineerTV = findViewById(R.id.configEngineerTextView);
+        engineerPlusBtn = findViewById(R.id.engineerPlusButton);
+        confirmationBtn = findViewById(R.id.playerConfigConfirmationButton);
+        skillPointsRemainingTV = findViewById(R.id.skillPointsRemainingTV);
+    }
+    //Called at start; activates listeners and initially disables buttons that
+    //shouldnbt be active from onCreate
+
+    private void initializeButtons(){
+        pilotMinusBtn.setEnabled(false);
+        fighterMinusBtn.setEnabled(false);
+        traderMinusBtn.setEnabled(false);
+        engineerMinusBtn.setEnabled(false);
+        pilotMinusBtn.setOnClickListener(this);
+        pilotPlusBtn.setOnClickListener(this);
+        fighterMinusBtn.setOnClickListener(this);
+        fighterPlusBtn.setOnClickListener(this);
+        traderMinusBtn.setOnClickListener(this);
+        traderPlusBtn.setOnClickListener(this);
+        engineerMinusBtn.setOnClickListener(this);
+        engineerPlusBtn.setOnClickListener(this);
+        confirmationBtn.setOnClickListener(this);
+
+    }
+    //Onclick methods for each Button
+    //0 = pilot
+    //1 = fighter
+    //2 = trader
+    //3 = engineer
+        @Override
+        public void onClick(View view){
+            switch(view.getId()){
+                case R.id.pilotMinusButton:
+                    changeSkill(0,false);
+                    Toast.makeText(ConfigurationActivity.this, "Woof", Toast.LENGTH_SHORT);
+                    break;
+                case R.id.pilotPlusButton:
+                    changeSkill(0,true );
+                    break;
+                case R.id.fighterMinusButton:
+                    changeSkill(1,false );
+                    break;
+                case R.id.fighterPlusButton:
+                    changeSkill(1, true);
+                    break;
+                case R.id.traderMinusButton:
+                    changeSkill(2, false);
+                    break;
+                case R.id.traderPlusButton:
+                    changeSkill(2, true);
+                    break;
+                case R.id.engineerMinusButton:
+                    changeSkill(3, false);
+                    break;
+                case R.id.engineerPlusButton:
+                    changeSkill(3, true);
+                    break;
+                case R.id.playerConfigConfirmationButton:
+                    break;
+            }
+        }
+
+    private void changeSkill(int skill, boolean isAdded){
+        boolean isMinusActive;
+        switch (skill){
+            case 0:
+                pilotSP = isAdded?pilotSP+1:pilotSP-1;
+
+                pilotTV.setText("Pilot Skill: " + pilotSP+ "");
+                isMinusActive = pilotSP > 0?true:false;
+                pilotMinusBtn.setEnabled(isMinusActive);
+                break;
+            case 1:
+                fighterSP = isAdded?fighterSP+1:fighterSP-1;
+                fighterTV.setText("Fighter Skill: " + fighterSP + "");
+                isMinusActive = fighterSP > 0?true:false;
+                fighterMinusBtn.setEnabled(isMinusActive);
+                break;
+            case 2:traderSP = isAdded?traderSP+1:traderSP-1;
+                traderTV.setText("Trader Skill: " + traderSP +"");
+                isMinusActive = traderSP > 0?true:false;
+                traderMinusBtn.setEnabled(isMinusActive);
+                break;
+            case 3:engineerSP = isAdded?engineerSP+1:engineerSP-1;
+                engineerTV.setText("Engineer Skill: " + engineerSP + "");
+                isMinusActive = engineerSP > 0?true:false;
+                engineerMinusBtn.setEnabled(isMinusActive);
+                break;
+        }
+        remainingPoints = isAdded?remainingPoints-1:remainingPoints+1;
+        skillPointsRemainingTV.setText("Skill Points Remaining: " + remainingPoints);
+        if(remainingPoints <= 0){
+            pilotPlusBtn.setEnabled(false);
+            fighterPlusBtn.setEnabled(false);
+            traderPlusBtn.setEnabled(false);
+            engineerPlusBtn.setEnabled(false);
+        }else{
+            pilotPlusBtn.setEnabled(true);
+            fighterPlusBtn.setEnabled(true);
+            traderPlusBtn.setEnabled(true);
+            engineerPlusBtn.setEnabled(true);
+        }
     }
 }
