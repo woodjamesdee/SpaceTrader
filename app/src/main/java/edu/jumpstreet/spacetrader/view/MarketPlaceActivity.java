0358c817a941ca7e20a9b8e1cd0a02f2deacfe55
MantisMcKoy <acfujimoto@gmail.com>
2019-03-02 15:48:23 -0500

    reorganized market Activity

--- app/src/main/java/edu/jumpstreet/spacetrader/view/MarketPlaceActivity.java
+++ app/src/main/java/edu/jumpstreet/spacetrader/view/MarketPlaceActivity.java
@@ -3,12 +3,14 @@ package edu.jumpstreet.spacetrader.view;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import edu.jumpstreet.spacetrader.R;
 import edu.jumpstreet.spacetrader.entity.Planet;
 import edu.jumpstreet.spacetrader.model.Model;
 
 public class MarketPlaceActivity extends AppCompatActivity {
+    //resource Layouts
     LinearLayout fullLayout;
     LinearLayout waterLayout;
     LinearLayout furLayout;
@@ -20,6 +22,10 @@ public class MarketPlaceActivity extends AppCompatActivity {
     LinearLayout machineLayout;
     LinearLayout narcoticsLayout;
     LinearLayout robotsLayout;
+
+    //Player determined Views
+    TextView remainingCreditsTV;
+    TextView reamainingCargoSpaceTV;
 
     Model model;
     Planet currentPlanet;
@@ -33,6 +39,7 @@ public class MarketPlaceActivity extends AppCompatActivity {
         currentPlanet = model.getGameInteractor().getActivePlanet();
         techLevel = currentPlanet.getTechLevel().ordinal();
         initializeLayouts();
+        initializeUserViews();
         deactiveResourceLayouts(techLevel);
     }
 
@@ -42,12 +49,24 @@ public class MarketPlaceActivity extends AppCompatActivity {
         furLayout = findViewById(R.id.furResourceLayout);
         foodLayout = findViewById(R.id.foodResourceLayout);
         oreLayout = findViewById(R.id.oreResourceLayout);
-        gamesLayout = findViewById(R.id.gameResourceLayout);
-        firearmsLayout = findViewById(R.id.firearmResourceLayout);
+        gamesLayout = findViewById(R.id.gamesResourceLayout);
+        firearmsLayout = findViewById(R.id.firearmsResourceLayout);
         medicineLayout = findViewById(R.id.medicineResourceLayout);
-        machineLayout = findViewById(R.id.machineResourceLayout);
-        narcoticsLayout = findViewById(R.id.narcoticResourceLayout);
-        robotsLayout = findViewById(R.id.robotResourceLayout);
+        machineLayout = findViewById(R.id.machinesResourceLayout);
+        narcoticsLayout = findViewById(R.id.narcoticsResourceLayout);
+        robotsLayout = findViewById(R.id.robotsResourceLayout);
+    }
+
+    private void initializeUserViews(){
+        remainingCreditsTV = findViewById(R.id.OnMarketUserCredits);
+        remainingCreditsTV.setText("Credits: " + model.getPlayerInteractor().getPlayerBalance());
+        reamainingCargoSpaceTV = findViewById(R.id.OnMarketCargoSpace);
+    }
+
+
+
+    private void initializeCostTextView(){
+
     }
 
     private void deactiveResourceLayouts(int techLevel){
@@ -71,4 +90,6 @@ public class MarketPlaceActivity extends AppCompatActivity {
     private void deactiveSellButtons(){
 
     }
+
+
 }
