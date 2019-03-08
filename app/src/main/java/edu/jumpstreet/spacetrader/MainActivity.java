4e97a36e60da9ee8feb3aed0e1e441aa4f87ef40
MantisMcKoy <acfujimoto@gmail.com>
2019-03-08 14:04:58 -0500

    added the main song to app; seems to have some issues interpolating the song

--- app/src/main/java/edu/jumpstreet/spacetrader/MainActivity.java
+++ app/src/main/java/edu/jumpstreet/spacetrader/MainActivity.java
@@ -1,11 +1,16 @@
 package edu.jumpstreet.spacetrader;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 import edu.jumpstreet.spacetrader.view.ConfigurationActivity;
 
@@ -17,6 +22,8 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         setContentView(R.layout.activity_main);
         ConstraintLayout layout = findViewById(R.id.mainConstrainLayout);
         layout.setOnTouchListener(this);
+        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.game_maintheme);
+        mediaPlayer.start();
     }
 
     public boolean onTouch(View v, MotionEvent event){
